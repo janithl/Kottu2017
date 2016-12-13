ae09c5520db7d2e23433d0c0b2b58f1bdd7c61c1
Janith <janith@yamu.lk>
2016-12-13 12:44:54 +0530

    added blogroll

--- kottu/views.py
+++ kottu/views.py
@@ -6,16 +6,25 @@ from kottu.models import Post, Blog
 PER_PAGE = 20 # items per page
 
 @app.route('/', defaults={'page': 1})
-@app.route('/page/<int:page>')
+@app.route('/page/<int:page>/')
 def index(page):
+	"""Renders the front page of Kottu, with all the latest posts"""
 	posts = Post.query.order_by(Post.id.desc()).paginate(page, PER_PAGE)
 	return render_template('article.html', title='All Posts', posts=posts)
 
 @app.route('/about/')
 def about():
+	"""Renders the Kottu about page"""
 	return render_template('about.html', title='About Kottu')
 
+@app.route('/blogroll/', defaults={'page': 1})
+@app.route('/blogroll/page/<int:page>/')
+def blogroll(page):
+	"""Renders the Kottu blogroll"""
+	blogs = Blog.query.order_by(Blog.name.asc()).paginate(page, PER_PAGE * 5)
+	return render_template('blogroll.html', title='Blogroll', blogs=blogs)
+
 @app.route('/blog/<int:blog_id>/')
-def show_blog_posts(blog_id):
+def blog(blog_id):
 	blog = Blog.query.get_or_404(id)
 	return blog.name #render_template('article.html', title=blog.name, posts=blog.posts)
