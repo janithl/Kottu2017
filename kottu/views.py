119134220277d7afdb13913dc21e1bef9ed9a0a8
Janith <janith@yamu.lk>
2016-12-13 12:16:35 +0530

    fixed pagination

--- kottu/views.py
+++ kottu/views.py
@@ -3,9 +3,12 @@ from flask import render_template, jsonify
 from kottu import app
 from kottu.models import Post, Blog
 
-@app.route('/')
-def index():
-	posts = Post.query.order_by(Post.id.desc()).paginate()
+PER_PAGE = 20 # items per page
+
+@app.route('/', defaults={'page': 1})
+@app.route('/page/<int:page>')
+def index(page):
+	posts = Post.query.order_by(Post.id.desc()).paginate(page, PER_PAGE)
 	return render_template('article.html', title='All Posts', posts=posts)
 
 @app.route('/blog/<int:blog_id>/')
