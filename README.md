b203d850bc9a433ebbfe7a15d861c35f45407707
Janith <janith@yamu.lk>
2016-12-13 21:20:10 +0530

    using instance for saving config, added pip freeze into requirements.txt, a readme and a license file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kottu2017
+
+A rewrite of [Kottu](https://github.com/janithl/Kottu2012), in 
+Python (Flask, SQLAlchemy etc.)
--- kottu/__init__.py
+++ kottu/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('kottu.cfg')
 
 import kottu.utils
